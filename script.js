c92203c08968b828c59e4e6de26b00f64a6c942b
sparksn1 <sparksn@bgsu.edu>
2024-11-06 10:14:35 -0500

    Waffle!!

--- script.js
+++ script.js
@@ -2,3 +2,8 @@
 //     const announcementText = "🎉 Next CTF challenge on Friday! Get ready to hack! 🎉";
 //     document.querySelector('.announcement-banner p').textContent = announcementText;
 // });
+
+function toggleMenu() {
+    const menu = document.querySelector('.navbar-menu');
+    menu.classList.toggle('active');
+}
