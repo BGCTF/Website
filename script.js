8d8ce5d0c5007ff38aff1ed8c2913dee6d379519
sparksn1 <sparksn@bgsu.edu>
2024-11-05 20:09:25 -0500

    The Start.

---
+++ script.js
@@ -0,0 +1,4 @@
+// document.addEventListener("DOMContentLoaded", function() {
+//     const announcementText = "🎉 Next CTF challenge on Friday! Get ready to hack! 🎉";
+//     document.querySelector('.announcement-banner p').textContent = announcementText;
+// });
